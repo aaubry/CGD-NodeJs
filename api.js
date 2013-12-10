7ebd3f7b61bf5daca173f372cd2e9886e78e099a
Antoine Aubry <aaubry@gmail.com>
2013-12-10 23:53:47 +0000

    Initial version

---
+++ api.js
@@ -0,0 +1,146 @@
+var request = require("request");
+
+var baseUrl = "https://app.caixadirecta.cgd.pt/apps/r/";
+
+var appHeaders = {
+	"X-CGD-APP-Device": "w8",
+	"X-CGD-APP-Version": "3.2"
+};
+
+function handle_error(handler, cb) {
+	return function (err, response, body) {
+		if(err) return cb(err);
+		if(response.statusCode != 200) {
+			console.warn(body);
+			return cb("HTTP status code " + response.statusCode, null);
+		}
+		handler(response, body);
+	};
+}
+
+function pad_number(val, length, padding) {
+	padding = padding || "0";
+	val = val.toString();
+	
+	while(val.length < length) {
+		val = padding + val;
+	}
+	return val;
+}
+
+function format_date(date, hour) {
+	hour = hour || "00:00:00";
+	
+	var year = pad_number(date.getFullYear(), 4);
+	var month = pad_number(date.getMonth() + 1, 2);
+	var day = pad_number(date.getDate(), 2);
+	return year + "-" + month + "-" + day + " " + hour;
+}
+
+function parse_date(val) {
+	return val.match(/^(\d{4}-\d{2}-\d{2})/)[1];
+}
+
+function parse_money(val) {
+	return pad_number(val, 3).replace(/^(\d*)(\d\d)$/, "$1.$2");
+}
+
+exports.authenticate = function(username, password, cb) {
+	
+	console.warn("Sending authentication request");
+	var auth = request.jar();
+	request({
+		url: baseUrl + "co/li?u=" + username,
+		headers: appHeaders,
+		jar: auth,
+		json: {},
+		auth: {
+			user: username,
+			pass: password
+		}
+	}, handle_error(authentication_complete, cb));
+	
+	function authentication_complete(response, body) {
+		console.warn("Successfully authenticated");
+		cb(null, auth);
+	}
+}
+
+exports.get_accounts = function(auth, cb) {
+	console.warn("Requesting account list");
+	request({
+		url: baseUrl + "pg/l",
+		headers: appHeaders,
+		jar: auth,
+		json: {}
+	}, handle_error(accounts_retrieved, cb));
+
+	function accounts_retrieved(response, body) {
+		console.warn("Account list retrieved");
+		
+		var accounts = [];
+		for(var name in body) {
+			body[name].forEach(function(e) {
+				e.ttl.forEach(function(l) {
+					l.cttl.forEach(function(a) {
+						accounts.push({
+							id: a.cntk,
+							name: a.cnt,
+							type: a.tc,
+							balance: parse_money(a.scnt)
+						});
+					});
+				});
+			});
+		}
+		
+		cb(null, accounts);
+	}
+}
+
+exports.get_movements = function(auth, account, startDate, endDate, chunkCb, cb) {
+	console.warn("Requesting movements of account " + account + " between " + format_date(startDate, "") + "and " + format_date(endDate, ""));
+	
+	var query = {
+		cnt: account,
+		dti: format_date(startDate, "00:00:00"),
+		dtf: format_date(endDate, "23:59:59")
+	};
+	
+	retrieve_next_page();
+
+	function retrieve_next_page() {
+		request({
+			url: baseUrl + "cnt/dc/m",
+			headers: appHeaders,
+			jar: auth,
+			method: "POST",
+			json: query
+		}, handle_error(movements_retrieved, cb));
+	}
+	
+	function movements_retrieved(response, body) {
+		console.warn("Movement chunk retrieved");
+		
+		var movements = body.lmov.map(function(m) {
+			return {
+				description: m.des,
+				date: parse_date(m.dt),
+				value_date: parse_date(m.dtv),
+				number: m.nmv,
+				amount: parse_money(m.mon),
+				type: m.tpm
+			};
+		});
+		
+		chunkCb(movements);
+		
+		if(body.lp) {
+			console.warn("All movements have been retrieved");
+			cb(null);
+		} else {
+			query.pkl = body.pkl;
+			retrieve_next_page();
+		}
+	}
+}
---
+++ app.js
@@ -0,0 +1,114 @@
+var program = require("commander");
+var prompt = require("prompt");
+var Table = require("easy-table");
+var api = require("./api");
+
+prompt.start();
+
+program
+	.version("0.0.1")
+	.option("-v, --verbose", "print additional information to stderr")
+	.option("-u, --user <user>", "authenticate as <user>");
+
+program
+	.command("accounts")
+	.description("List accounts")
+	.action(authenticated(list_accounts));
+
+program
+	.command("movements <account>")
+	.description("Retrieve movements")
+	.option("-s, --start-date <startDate>", "get movements starting from <startDate>", parse_date)
+	.option("-e, --end-date <endDate>", "get movements starting from <endDate>", parse_date)
+	.action(authenticated(retrieve_movements));
+
+program.parse(process.argv);
+
+function parse_date(val) {
+	var match = val.match(/^(\d{4})[\-\/\.]?(\d{1,2})[\-\/\.]?(\d{1,2})$/)
+	if(match != null) {
+		var year = parseInt(match[1]);
+		var month = parseInt(match[2]) - 1;
+		var day = parseInt(match[3]);
+		
+		var date = new Date(year, month, day);
+		var isValid = date.getFullYear() == year
+			&& date.getMonth() == month
+			&& date.getDate() == day;
+		
+		return isValid ? date : null;
+	}
+	return null;
+}
+
+function print_table(val) {
+	var tbl = new Table();
+	val.forEach(function(a) {
+		for(var name in a) {
+			tbl.cell(name, a[name]);
+		}
+		tbl.newRow();
+	});
+	
+	console.log(tbl.toString());
+}
+
+function authenticated(action) {
+	return function() {
+		var args = Array.prototype.slice.call(arguments);
+		
+		if(program.verbose != true) {
+			console.warn = function() {}
+		}
+		
+		var fields = [];
+		if(program.user == null) {
+			fields.push({
+				name: "username",
+				required: true
+			});
+		}
+		
+		fields.push({
+			name: "password",
+			required: true,
+			hidden: true
+		});
+		
+		prompt.get(fields, function(err, res) {
+			if(err) return console.error("ERROR", err);
+		
+			if(res.username) program.user = res.username;
+			
+			api.authenticate(res.username || program.user, res.password, function(err, auth) {
+				if(err) return console.error("ERROR", err);
+
+				args.push(auth);
+				action.apply(this, args);
+			});
+		});
+	}
+}
+
+function list_accounts(args, auth) {
+	api.get_accounts(auth, function(err, accounts) {
+		if(err) return console.error("ERROR", err);
+		print_table(accounts);
+	});
+}
+
+function retrieve_movements(account, args, auth) {
+	var endDate = args.endDate || new Date();
+	var startDate = args.startDate || new Date(endDate.getFullYear(), endDate.getMonth() - 1, endDate.getDate());
+	
+	api.get_movements(auth, account, startDate, endDate, chunk_retrieved, movements_retrieved);
+	
+	function chunk_retrieved(movements) {
+		print_table(movements);
+	}
+	
+	function movements_retrieved(err) {
+		if(err) return console.error("ERROR", err);
+		console.warn("Done");
+	}
+}
